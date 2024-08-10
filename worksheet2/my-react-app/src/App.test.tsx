459e7006828644978389a24522d312bdfd561c07
y7k9h8d9 <56863443+y7k9h8d9@users.noreply.github.com>
2024-08-10 14:59:19 +1200

    App.js changed

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -6,4 +6,10 @@ const div = document.createElement('div');
 ReactDOM.render(<App />, div);
 ReactDOM.unmountComponentAtNode(div);
 });
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+  expect(2 + 2).toBe(5);
+  });
+  });
+  
 
